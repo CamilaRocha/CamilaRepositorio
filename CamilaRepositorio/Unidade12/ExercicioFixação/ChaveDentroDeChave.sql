3ee2008c82f758cc2e950e6d384d4de62fafa8b2
caa96 <camila_caah96@hotmail.com>
2014-06-10 11:48:18 -0300

    Atualizando Banco de Dados

---
+++ CamilaRepositorio/Unidade12/ExercicioFixação/ChaveDentroDeChave.sql
@@ -0,0 +1,13 @@
+﻿--Criando Chave , dentro de chave
+
+CREATE TABLE [dbo].[Livro]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Titulo] VARCHAR(50) NULL, 
+    [AnoPublicacao] DATE NULL, 
+    [Autor_Id] INT NOT NULL,
+	FOREIGN KEY (Autor_Id) REFERENCES Autor(Id)
+
+
+
+)
---
+++ CamilaRepositorio/Unidade12/ExercicioFixação/Exercicio10.sql
@@ -0,0 +1,5 @@
+﻿--Mostra Titulo e autor
+
+
+Select L.Titulo, L.AnoPublicacao, A.Nome from Livro as L, Autor as A
+Where A.Id = L.Autor_Id

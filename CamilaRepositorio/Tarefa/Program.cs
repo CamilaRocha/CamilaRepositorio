9d0e04c21fe45a9a8269bbcef201064c4368b1bd
caa96 <camila_caah96@hotmail.com>
2014-06-23 11:22:00 -0300

    Adicionando Exercicio de ArrayList

---
+++ CamilaRepositorio/Tarefa/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+
+namespace CollectionApplication
+{
+    class Program
+    {
+        static void Main1(string[] args)
+        {
+            ArrayList al = new ArrayList();
+
+            al.Add(145);
+            al.Add(718);
+            al.Add(333);
+            al.Add(5556);
+            al.Add(124);
+            al.Add(232);
+            al.Add(98);
+            al.Add(0);
+            al.Add(6);
+
+            Console.WriteLine("Capacidade do array: {0} ", al.Capacity);
+            Console.WriteLine("Total adicionado: {0}", al.Count);
+            
+
+            Console.Write("Numeros adicionados:  ");
+            foreach (int i in al)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.Write("Ordem crescente: ");
+            al.Sort();
+            foreach (int i in al)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+            Console.ReadKey();
+        }
+    }
+}
